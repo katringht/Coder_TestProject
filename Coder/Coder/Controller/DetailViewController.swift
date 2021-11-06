ecbecbf0567efbcba488a8c07dd838a264ecc0f1
katringht <katrin_tko@mail.ru>
2021-11-06 10:03:42 +0300

    added icon and made additionals to detailView

--- Coder/Coder/Controller/DetailViewController.swift
+++ Coder/Coder/Controller/DetailViewController.swift
@@ -15,10 +15,21 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var infoUserNameLabel: UILabel!
     @IBOutlet weak var infoUserImageView: UIImageView!
     
+    var info: [InfoUser] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         infoTableView.dataSource = self
+        setUpTableViewCell()
     }
+    
+    func setUpTableViewCell() {
+        let phoneImage = UIImage(systemName: "phone")
+        let starImage = UIImage(systemName: "star")
+        
+        info = [InfoUser(icon: starImage!, mainInfo: "Ekaterina Tarasova", extraInfo: "20"), InfoUser(icon: phoneImage!, mainInfo: "89601313296", extraInfo: "")]
+    }
+    
     @IBAction func backToButton(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     } 
@@ -28,12 +39,14 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell", for: indexPath) as! InformationTableViewCell
-//        let i = info[indexPath.row]
-//        cell.informationLabel.text = i.mainInfo
+        let i = info[indexPath.row]
+        cell.informationLabel.text = i.mainInfo
+        cell.icon.image = i.icon
+        cell.deteilInfoLabel.text = i.extraInfo
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return info.count
     }
 }
