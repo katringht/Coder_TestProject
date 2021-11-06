9a30e2d295fea6b34582d9f65557395cbe36fff3
katringht <katrin_tko@mail.ru>
2021-11-06 10:39:02 +0300

    added view for showing errors

---
+++ Coder/Coder/Controller/ShowErrorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ShowErrorViewController.swift
+//  Coder
+//
+//  Created by Ekaterina Tarasova on 06.11.2021.
+//
+
+import UIKit
+
+class ShowErrorViewController: UIViewController {
+
+    @IBOutlet weak var errorRefreshButton: UIButton!
+    @IBOutlet weak var errorSublable: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var errorIcon: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
